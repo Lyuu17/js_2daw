b6dd6a160429be103a8a94f740d68ca94042d3e3
lyuu17 <lyuu17@yandex.com>
2022-12-01 12:26:35 +0100

    Advent of Code 2022 day 1

---
+++ Advent of Code/2022/Day 1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-1/4 m-auto text-center">
+    <SubirLista @listaTexto="listaTexto"/>
+    <p>{{ lista[0] }}</p>
+  </div>
+</template>
+
+<script setup>
+import SubirLista from './components/SubirLista.vue'
+
+import { ref } from 'vue';
+
+const lista = ref([]);
+
+const listaTexto = (texto) => {
+
+  lista.value = 
+    texto
+      .split("\n\n")
+      .map(str => str.includes("\n") ? str.split("\n").map(e => e = parseInt(e)).reduce((acc, v) => acc + v, 0) : parseInt(str))
+      .sort((a, b) => b - a);
+  
+  console.log(lista.value);
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Advent of Code/2022/Day 1/src/components/SubirLista.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-5">
+    <input ref="entrada" type="file"/>
+    <button 
+      class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="procesar"
+      >
+      Procesar
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const entrada = ref();
+
+const emit = defineEmits(["listaTexto"]);
+
+const procesar = () => {
+  const file = entrada.value.files[0];
+  const reader = new FileReader();
+
+  reader.readAsText(file, "utf8");
+  reader.onload = () => {
+    const res = reader.result.trim();
+
+    emit("listaTexto", res);
+  };
+  reader.onerror = () => {
+    console.log(render.error);
+  };
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
